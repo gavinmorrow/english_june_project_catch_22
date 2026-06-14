a84fd63fe89e8a523a95320accb78ff07b13b906
Gavin Morrow <gavinfmorrow@gmail.com>
2026-06-14 16:54:12 -0400

    nicer curve

--- script.js
+++ script.js
@@ -11,30 +11,19 @@ const map = {
     const offsetFromTopOfMap = scrollY - wrapper.offsetTop;
     const scrollPercent = offsetFromTopOfMap / this.elem.clientHeight;
 
-    if (scrollPercent >= 0 && scrollPercent <= 1) {
+    if (0 <= scrollPercent && scrollPercent <= 1) {
       const ribbon = NonNull(document.getElementById("ribbon"));
       ribbon.style.opacity = `${scrollPercent}`;
       // Math.max is for overscroll, where the value becomes negative
       ribbon.style.marginTop = `${(Math.random() * 1 - 0.5) * (Math.max(0, this.elem.clientHeight - offsetFromTopOfMap) / (innerHeight / 5)) ** 4}px`;
     }
-    if (scrollPercent >= 1 && scrollPercent <= 2) {
+    if (1 <= scrollPercent && scrollPercent <= 3) {
       const bologna = NonNull(document.getElementById("bologna"));
-      bologna.style.setProperty(
-        "--overlay-opacity",
-        `${(((scrollPercent - 1) * 7) / 8) ** 2}`,
-      );
+      const opacity = ((-((scrollPercent - 2) ** 2) + 1) * 7) / 8;
+      bologna.style.setProperty("--overlay-opacity", `${opacity}`);
       const bolognaPin = NonNull(document.getElementById("bologna-pin"));
-      if (scrollPercent >= 1.5) bolognaPin.classList.add("big");
-      else bolognaPin.classList.remove("big");
-    }
-    if (scrollPercent >= 2 && scrollPercent <= 3) {
-      const bologna = NonNull(document.getElementById("bologna"));
-      bologna.style.setProperty(
-        "--overlay-opacity",
-        `${(((3 - scrollPercent) * 7) / 8) ** 2}`,
-      );
-      const bolognaPin = NonNull(document.getElementById("bologna-pin"));
-      if (scrollPercent <= 2.5) bolognaPin.classList.add("big");
+      if (1.5 <= scrollPercent && scrollPercent <= 2.5)
+        bolognaPin.classList.add("big");
       else bolognaPin.classList.remove("big");
     }
   },
@@ -42,24 +31,21 @@ const map = {
 
 const pages = [map];
 
-const intersectionObserver = new IntersectionObserver(
-  (entries, _observer) => {
-    for (const entry of entries) {
-      let atLeastOnePage = false;
-      for (const page of pages) {
-        if (entry.target == page.elem) {
-          page.isVisible = entry.isIntersecting;
-          atLeastOnePage = true;
-        }
-      }
-      if (!atLeastOnePage) {
-        console.error("Unknown target", { target: entry.target });
-        throw new Error("Unknown target");
+const intersectionObserver = new IntersectionObserver((entries, _observer) => {
+  for (const entry of entries) {
+    let atLeastOnePage = false;
+    for (const page of pages) {
+      if (entry.target == page.elem) {
+        page.isVisible = entry.isIntersecting;
+        atLeastOnePage = true;
       }
     }
-  },
-  { threshold: [1] },
-);
+    if (!atLeastOnePage) {
+      console.error("Unknown target", { target: entry.target });
+      throw new Error("Unknown target");
+    }
+  }
+});
 for (const page of pages) {
   intersectionObserver.observe(page.elem);
 }
